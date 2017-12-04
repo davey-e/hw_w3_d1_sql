a0a43fdc77008f9900bed9e67bf563827656ea1b
Dave Ellis <dave@ellisnetmail.co.uk>
2017-12-04 16:57:32 +0000

    Added SQL for questions 7, 8 and 9

--- marvel_queries.sql
+++ marvel_queries.sql
@@ -20,18 +20,22 @@ WHERE title = 'Batman Begins';
 SELECT * FROM movies;
 
 -- 6. Create a new entry in the 'people' table with the name of one of the instructors
-INSERT INTO people (name) VALUES ('Craig Morton');
-SELECT name FROM people;
+--INSERT INTO people (name) VALUES ('Craig Morton');
+--SELECT name FROM people;
 
 -- 7. Alex has decided to hijack our movie evening, Remove him from the table of people.
-
-
+DELETE FROM people
+WHERE name = 'Alex Bazlinton';
+SELECT name FROM people;
 
 -- 8. The cinema has just heard that they will be holding an exclusive midnight showing of 'Guardians of the Galaxy 2'!! Create a new entry in the 'movies' table to reflect this.
-
-
+INSERT INTO movies (title, year, show_time) VALUES ('Guardians of the Galaxy 2', 2017, '00:00');
+SELECT * FROM movies;
 
 -- 9. The cinema would also like to make the Guardian movies a back to back feature. Update the 'Guardians of the Galaxy' show time from 17:55 to 21:30
+UPDATE movies SET show_time = '21:30'
+WHERE title = 'Guardians of the Galaxy' AND year = 2014 AND show_time = '17:55';
+SELECT * FROM movies;
 
 -- ## Extension
 -- 1. Research how to delete multiple entries from your table in a single command.
